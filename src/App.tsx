38ff0066f9bf717327f37a5282bc8de8bcc15098
bolodissenoura <suamaemedeu@outlook.com>
2022-09-15 02:03:29 -0300

    feat: set title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="Logo escrito Esports" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.
+      </h1>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        background: "url('/bkg.png')"
+        background: "url('/bkg.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 51.94%, #E1D55D 74.57%)'
       },
     },
   },
